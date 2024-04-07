92fc77230e7e55c0e56dfca76827bb1a8daab834
Petty Wu <151545718+petty-wu@users.noreply.github.com>
2024-04-07 14:35:55 +0100

    Update client_details.py
    Rename 'ask_name' to 'client_name'

--- client_details.py
+++ client_details.py
@@ -3,8 +3,10 @@
 2. input user's age then print it out
 '''
 
-ask_name = "What is your name"
-name = input(ask_name + " ? ")
+#ask_name = "What is your name"
+client_name = "What is your name"
+#name = input(ask_name + " ? ")
+name = input(client_name + " ? ")
 print(f"Hi {name}!")
 
 ask_age = "How old are you"
