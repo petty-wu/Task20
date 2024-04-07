4ede6153073d1655569acb300ff602525aeee99c
Petty Wu <151545718+petty-wu@users.noreply.github.com>
2024-04-07 15:05:53 +0100

    Update client_details.py

--- client_details.py
+++ client_details.py
@@ -2,6 +2,7 @@
 1. input user's name then print it out
 2. input user's age then print it out
 '''
+adult_age = 21
 
 #ask_name = "What is your name"
 client_name = "What is your name"
@@ -11,4 +12,7 @@ print(f"Hi {name}!")
 
 ask_age = "How old are you"
 age = input(f"{ask_age} {name}? ")
-print("Happy year " + age + "!")
+if int(age) > adult_age:
+    print("Happy year " + age + "!")
+else:
+    print("Good year " + age + "!")
