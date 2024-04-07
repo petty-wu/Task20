0f3b61c2282440fdb7885a39c9b56405d8a40a31
Petty Wu <151545718+petty-wu@users.noreply.github.com>
2024-04-07 14:12:16 +0100

    Creat Client details

---
+++ client_details.py
@@ -0,0 +1,12 @@
+'''
+1. input user's name then print it out
+2. input user's age then print it out
+'''
+
+ask_name = "What is your name"
+name = input(ask_name + " ? ")
+print(f"Hi {name}!")
+
+ask_age = "How old are you"
+age = input(f"{ask_age} {name}? ")
+print("Happy year " + age + "!")
